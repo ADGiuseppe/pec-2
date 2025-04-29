4720388463e87cd1bb99db54e3e232064d164263
Alice Di Giuseppe <adigiuseppe@uoc.edu>
2025-04-29 14:42:48 +0200

    Use PurgeCSS as second dependency

---
+++ postcss.config.cjs
@@ -0,0 +1,10 @@
+const purgecss = require('@fullhuman/postcss-purgecss').default;
+
+module.exports = {
+  plugins: [
+    purgecss({
+      content: ['./src/**/*.html', './src/**/*.js'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
+  ]
+};
